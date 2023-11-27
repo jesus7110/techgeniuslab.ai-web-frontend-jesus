073b7fdb3dcbb8f47705bf7c220137d869907dee
jesus7110 <juthikahazra11@gmail.com>
2023-11-27 19:40:30 +0530

    bg-white

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -18,7 +18,7 @@ const navigation = [
 const Navbar2 = () => {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   return (
-    <header className="absolute inset-x-0 top-0 z-50">
+    <header className="absolute inset-x-0 top-0 z-50 bg-white">
         <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
