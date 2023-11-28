3e7e5f292fa9655598105711a40f3645d5c31aa8
jesus7110 <juthikahazra11@gmail.com>
2023-11-28 13:14:50 +0530

    workshop

--- src/components/Workshop.jsx
+++ src/components/Workshop.jsx
@@ -35,20 +35,28 @@ const posts = [
       },
   
   ]
+
+  const workshop = [
+    {
+      id: 1,
+      title: 'Begin with Arduino',
+      
+    }
+  ]
   
   export default function Workshop() {
     return (
       <>
-      <div className="bg-white py-24 sm:py-32">
+      <div className="bg-white py-24 sm:py-20">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
-         {/* <div className="mx-auto max-w-2xl lg:mx-0">
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Explore Our Engaging Workshops</h2>
+         <div className="mx-auto max-w-2xl lg:mx-0">
+            <h2 className="text-xl font-bold tracking-tight text-gray-900 sm:text-3xl">Explore Our Engaging Workshops</h2>
             <p className="mt-2 text-lg leading-8 text-gray-600">
             Unlocking Creativity and Learning Through IoT and Robotics
             </p>
-    </div>*/}
-             <h3 className="mt-3 text-xl font-semibold text-gray-900 group-hover:text-gray-600">Build Cool Projects, And prepare for your Future! </h3>
-            
+    </div>
+            {/*  <h3 className="mt-3 text-xl font-semibold text-gray-900 group-hover:text-gray-600">Build Cool Projects, And prepare for your Future! </h3>
+            */}
           <div className=" mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
             {posts.map((post) => (
               <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
