36db9dbd0e6d49cc9f33205303af4f744767603c
jesus7110 <juthikahazra11@gmail.com>
2023-11-29 11:37:50 +0530

    workshop.js - courses

--- src/components/Workshop.jsx
+++ src/components/Workshop.jsx
@@ -4,6 +4,7 @@ import wc1 from '../assets/wc1.png'
 import wc2 from '../assets/wc2.png'
 import wc3 from '../assets/wc3.png'
 import wc4 from '../assets/wc4.png'
+import banner from '../assets/appbanner.png'
 
 const posts = [
     {
@@ -345,7 +346,7 @@ const posts = [
         
       <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
       <img
-        src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+        src={banner}
         alt=""
         className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
       />
