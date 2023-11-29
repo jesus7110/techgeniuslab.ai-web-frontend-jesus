a6af3f868d52d69a9ee9f675b1f40a2f0ed9a2dd
jesus7110 <juthikahazra11@gmail.com>
2023-11-29 16:33:28 +0530

    class

---
+++ src/config/FirebaseConfig.js
@@ -0,0 +1,20 @@
+// Import the required Firebase modules
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+
+// Replace with your Firebase project config
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process,
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  appId: 'YOUR_APP_ID',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Export Firestore for use in components
+export const firestore = firebase.firestore();
