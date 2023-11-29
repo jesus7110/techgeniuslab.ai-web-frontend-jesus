ce3e6a710fd24da3a47f96aa2f9ccb7efa124776
jesus7110 <juthikahazra11@gmail.com>
2023-11-29 16:35:08 +0530

    class

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -7,7 +7,7 @@ import 'firebase/firestore';
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.,
+  projectId: process.env.REACT_APP_projectId,
   storageBucket: 'YOUR_STORAGE_BUCKET',
   messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
   appId: 'YOUR_APP_ID',
