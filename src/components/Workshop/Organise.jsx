8035dfbadf3aabf71ba7ce8f7269f22142388112
Savinrai <salvinrai24@gmail.com>
2024-02-14 19:30:58 +0530

    workshop page updated

--- src/components/Workshop/Organise.jsx
+++ src/components/Workshop/Organise.jsx
@@ -4,7 +4,7 @@ const Organise = () => {
   return (
     <div className="bg-white">
     <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
-      <div className="relative isolate overflow-hidden bg-red-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+      <div className="relative isolate overflow-hidden bg-red-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24  lg:px-24 lg:pt-0">
         <svg
           viewBox="0 0 1024 1024"
           className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
@@ -18,18 +18,16 @@ const Organise = () => {
             </radialGradient>
           </defs>
         </svg>
-        <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+        <div className=" text-center lg:w-4/5  lg:py-32 lg:text-left">
           <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
           Interested in hosting a workshop tailored to your specific needs? 
            
           </h2>
           <p className="mt-6 text-lg leading-8 text-gray-100">
-          We're here to help! Fill out the form below to request a customized workshop in STEM, IoT, or AI. 
+          We're here to help! Fill out the form below to request a customized workshop in STEM, IoT, or AI. Whether you're a school, college, or organisation, our team will work with you to design a workshop that meets your objectives and exceeds your expectation.
           </p>
 
-          <p className="mt-6 text-lg leading-8 text-gray-100">
-          We're here to help! Fill out the form below to request a customized workshop in STEM, IoT, or AI. 
-          </p>
+          
           <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
             <a
               href="/requestworkshop"
@@ -42,9 +40,7 @@ const Organise = () => {
             
           </div>
         </div>
-        <div className="relative mt-16 h-80 lg:mt-8">
-          
-        </div>
+        
       </div>
     </div>
   </div>
--- src/components/stemLab/feature.jsx
+++ src/components/stemLab/feature.jsx
@@ -2,51 +2,35 @@ import React from 'react';
 
 const ComponentCard = ({ title, description }) => (
   <div className="p-8 space-y-3 border-2 border-indigo-800 rounded-xl shadow-2xl shadow-indigo-100">
-    {/* {<span className="inline-block text-blue-500 dark:text-blue-400">
-      <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z" />
-      </svg>
-    </span>} */}
-
-    <h1 className="text-2xl font-semibold text-gray-700 capitalize ">{title}</h1>
-
-    <p className="text-gray-600">
-      {description}
-    </p>
-
-  
+    <h1 className="text-xl font-semibold text-gray-700 capitalize">{title}</h1>
+    <p className="text-gray-600 text-justify px-1.5">{description}</p>
   </div>
 );
 
 const FeatureSection = () => (
   <div className="relative isolate overflow-hidden bg-white py-16 sm:py-24 lg:py-16">
-    <div className="mx-auto max-w-7xl pt-10 pb-20 px-6 lg:px-8">
-    <div className="max-w-6xl lg:max-6w-lg">
-    <h2 className="text-base font-semibold leading-7 text-red-600">Objectives of STEM Lab</h2>
-    <h2 className="text-xl font-bold tracking-tight text-gray-900 sm:text-3xl ">The primary objective of establishing the STEM Lab is to provide a dynamic learning environment that facilitates hands-on experience and skill development in AI & IoT technology
- </h2>
-          
-        </div>
-      <div className="mx-auto grid max-w-2xl pt-12 grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-1">
-        
-        <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-4 lg:pt-2">
-          <ComponentCard
-            title="Handpicked Course"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet"
-          />
-          <ComponentCard
-            title="Lab Setup & Equipments"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet"
-          />
-          <ComponentCard
-            title="Expert Sessions & Mentorship"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet"
-          />
-          <ComponentCard
-            title="Student Learning Portal"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet"
-          />
-        </dl>
+    <div className="mx-auto max-w-7xl pt-10 pb-20 px-2 lg:px-2 w-4/5">
+      <div className="max-w-6xl lg:max-w-6w-lg">
+        <h2 className="text-base font-semibold leading-7 text-red-600">Objectives of STEM Lab</h2>
+        <h2 className="text-xl font-bold  tracking-tight text-gray-900 sm:text-3xl">The primary objective of establishing the STEM Lab is to provide a dynamic learning environment that facilitates hands-on experience and skill development in AI & IoT technology</h2>
+      </div>
+      <div className="mx-auto grid max-w-2xl pt-12 grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1">
+        <ComponentCard
+          title="Handpicked Course"
+          description="Explore our carefully curated STEM courses, designed to pique students' attention, develop critical thinking, and provide them with necessary skills for the future. Our evaluation parameters enable teachers to steer their students' progress.."
+        />
+        <ComponentCard
+          title="Lab Setup & Equipments"
+          description="Enhance your educational landscape with our comprehensive STEM Lab Setup service. Our commitment consists of six months of focused teacher development, workshops, and interesting competitions and post lab setup support."
+        />
+        <ComponentCard
+          title="Expert Sessions & Mentorship"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet"
+        />
+        <ComponentCard
+          title="Student Learning Portal"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet"
+        />
       </div>
     </div>
     <div className="absolute left-1/2 top-0 -z-10 -translate-x-1/2 blur-3xl xl:-top-6" aria-hidden="true">
