fa0a2c05984bbfd5f6747957afab92d3c1274d01
jesus7110 <juthikahazra11@gmail.com>
2024-02-16 14:11:49 +0530

    service page

--- src/components/Sevice/servicemain.jsx
+++ src/components/Sevice/servicemain.jsx
@@ -608,28 +608,14 @@ const About1 = () => {
 
             <div className="w-full mr-20 ml-4 lg:w-1/2 xl:w-5/12">
               <div className="mt-10 lg:mt-0">
-                <span className="block mb-4 text-lg font-semibold text-primary">
-                  Why Choose Us
-                </span>
-                <h2 className="mb-5 text-3xl font-bold text-dark dark:text-white sm:text-[40px]/[48px]">
-                  Make your customers happy by giving services.
-                </h2>
-                <p className="mb-5 text-base text-body-color dark:text-dark-6">
-                  It is a long established fact that a reader will be distracted
-                  by the readable content of a page when looking at its layout.
-                  The point of using Lorem Ipsum is that it has a more-or-less.
-                </p>
-                <p className="mb-8 text-base text-body-color dark:text-dark-6">
-                  A domain name is one of the first steps to establishing your
-                  brand. Secure a consistent brand image with a domain name that
-                  matches your business.
-                </p>
-                <a
-                  href="javascript:void(0)"
-                  className="inline-flex items-center justify-center py-3 text-base font-medium text-center text-white border border-transparent rounded-md px-7 bg-primary hover:bg-opacity-90"
-                >
-                  Get Started
+              <h1 className="text-xl font-bold tracking-tight text-gray-900 sm:text-5xl py-8">
+                Leverage Our Digital Solutions to Bring Digitization to Your Business
+                </h1>
+                
+                <a href="/form" className="inline-block sm:rounded-full  mt-10 border border-transparent bg-red-700 px-8 py-3 text-center font-medium text-white hover:bg-slate-800">
+                Talk to expert
                 </a>
+
               </div>
             </div>
           </div>
